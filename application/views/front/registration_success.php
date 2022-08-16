a4c61f2125f0b6e17d3d310d575e5297bcff18b3
Purvi597 <trivedipurvi593@gmail.com>
2022-08-17 00:21:22 +0530

    update registration form

--- application/views/front/registration_success.php
+++ application/views/front/registration_success.php
@@ -11,13 +11,14 @@
 		
 	<div class="page-banner">
         <img class="animation-element fadeUp animation-element-fast" src="<?= base_url(); ?>assets/img/form-submission.jpg" alt="About Us">
-		<h1 class="animation-element fadeUp animation-element-exslow"> Successfully </h1>
     </div>
 	
    <div class="container">
 	  <center>
-		 <p style="color: green;">YOUR REGISTRATION HAS BEEN SUCCESSFULLY</p>
+		<div class="success-msg" style="margin-bottom: 60px;">
+		 <p style="color: green;padding-top:30px;">YOUR REGISTRATION HAS BEEN SUCCESSFULLY</p>
 		 <a href="<?= base_url() ?>" class="btn book-now-btn form_btn">EXIT</a><?php if($record->category == 1 || $record->category == 2 || $record->category == 3){$link = 'storeprofile-setting/'.$record->id;}else{$link = 'profile-setting/'.$record->id;} ?>  <a href="<?= base_url().$link ?>" class="btn book-now-btn form_btn">GO TO PROFILE</a>
+		</div>
 	  </center>
    </div>
 	
